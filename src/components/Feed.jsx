1addde67c06ba3dc542a301f7666663be7a9ca9c
Somnath Dey <sdey.aklakhi@gmail.com>
2023-03-11 09:42:00 +0530

    fetched API data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,18 @@ import Videos from "./Videos";
 
 import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -16,7 +27,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -33,10 +47,11 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New<span style={{ color: "#F31503" }}>videos</span>
+          {selectedCategory}
+          <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
